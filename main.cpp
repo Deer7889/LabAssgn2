25ef96b0f45a8579331229a10dcb2f5569d1f4fd
MrCr <oak7887@gmail.com>
2023-04-26 12:57:09 +0000

    x
    Signed-off-by: MrCr <oak7887@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include "mbed.h"   //call mbed library
 
-int nth_term = 9;   //set the Fibonacci sequence length to 9
-int term_1, term_2; //set integers named term_1 and term_2     
-int next_term = 0;  //set integer next_term to 0
+int nth_term = 9;   //define the Fibonacci sequence length as 9
+int term_1, term_2; //define integers named term_1 and term_2     
+int next_term = 0;  //define and set integer next_term to 0
 
 int main() {        //start main program using a simple for loop
     term_1 = 0;       //set term_1 to 0
     term_2 = 1;       //set term_\2 to 1
     printf("Fibonacci Number sequence length is %d \r\n", nth_term);    //print  information and number of terms
     printf("0, 1, ");                                     //print 0,1 always for sequence start
-        for(int i = 2; i<nth_term;i++) {  //set i to 2 and increase by 1 for sequence, start for loop
+        for(int i = 2; i<nth_term;i++) {  //start for loop, if set i to 0, check if under 9, increase by 1
         next_term = term_1+term_2;          //add two terms and store as next_term
         printf("%d, ",next_term);           //print the next_term value , without going to new line
         term_1 = term_2;                      //set term_1 to new vale for next sequence step
